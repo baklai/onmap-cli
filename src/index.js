23e77094b0309a7c4041227bb87ff25688e7c8f1
Dmitrii <d.n.baklai@gmail.com>
2022-02-03 20:20:17 +0200

    async scanner function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const scanner = require('./utils/onmap-scan');
+const onmap = require('./utils/onmap-scan');
 const csvToArray = require('./utils/csv-to-array');
 
-module.exports = { scanner, csvToArray };
+module.exports = { onmap, csvToArray };
--- src/utils/onmap-scan.js
+++ src/utils/onmap-scan.js
@@ -19,37 +19,42 @@ const { MONGO_URI } = require('../conf');
 
 const Report = require('../models/onmap.model');
 
-module.exports = async (title, flags, targets, userID) => {
-  const opts = {
-    json: true,
-    timeout: 300,
-    range: targets,
-    flags: flags
-  };
-  consola.info(
-    `Scan started for ${targets.join()} at ${new Date().toLocaleString()}`
-  );
-  nmap.scan(opts, async (err, report) => {
-    if (err) {
-      consola.error(err);
-      process.exit(0);
-    } else {
-      for (let item in report) {
-        await Report.findOneAndUpdate(
-          { target: item },
-          {
-            title: title || 'ONMAP CLI Scan',
-            target: item,
-            flags: opts.flags || [],
-            ...report[item],
-            userID: userID ? userID : null
-          },
-          { new: true, upsert: true, useFindAndModify: false }
-        );
-        consola.success(
-          `Scan success ${item} at ${new Date().toLocaleString()}`
-        );
+const onmap = async (title, flags, targets) => {
+  return await new Promise((resolve, reject) => {
+    const opts = {
+      json: true,
+      timeout: 300,
+      range: targets,
+      flags: flags
+    };
+    consola.info(
+      `Scan started for ${targets.join()} at ${new Date().toLocaleString()}`
+    );
+    nmap.scan(opts, async (err, report) => {
+      if (err) {
+        reject(err);
+      } else {
+        for (let item in report) {
+          await Report.findOneAndUpdate(
+            { target: item },
+            {
+              title: title || 'ONMAP CLI Scan',
+              target: item,
+              flags: opts.flags || [],
+              ...report[item]
+            },
+            { new: true, upsert: true, useFindAndModify: false }
+          );
+          consola.success(
+            `Scan success for ${item} at ${new Date().toLocaleString()}\n`
+          );
+        }
+        resolve(true);
       }
-    }
+    });
   });
+};
+
+module.exports = async (title, flags, targets) => {
+  await onmap(title, flags, targets);
 };
